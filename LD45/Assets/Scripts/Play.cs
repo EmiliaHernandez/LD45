ff7f4781ea5718f9ff77e0d1abbce0b9dd5edca9
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2019-10-04 21:24:03 -0400

    Fixing up MENU

---
+++ LD45/Assets/Scripts/Play.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+
+    public void Playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Aboutgame()
+    {
+        gameObject.enabled = true;
+    }
+
+    public void hideabout()
+    {
+        gameObject.enabled = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
